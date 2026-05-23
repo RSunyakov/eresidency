185f1c8ea87630763ff5b0c12ebea3a997581a86
Roman Syunyakov <rsyunyakov@ibs.ru>
2026-05-24 00:50:52 +0300

    English labels, anonymous defaults, flag radius 8, no crop

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2445792508" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1310422881" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
